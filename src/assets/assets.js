c0e043c5add50610a9447515a2aeb4801a155636
raksha703 <703raksha@gmail.com>
2025-07-10 14:38:39 +0530

    added

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,6 +2,10 @@ import logo from "../assets/logo.png";
 import car from "../assets/car.jpg";
 import bike from "../assets/bike.jpg";
 import scooty from "../assets/scooty.jpg";
+import feature1 from "../assets/feature1.jpg";
+import feature2 from "../assets/feature2.jpg";
+import feature3 from "../assets/feature3.jpg";
+import instructorimg from "../assets/instructor.jpg";
 import bg from "../assets/bg1.jpg"
 
-export {car, bike, scooty, logo, bg};
+export {car, bike, scooty, feature1, feature2, feature3, logo, instructorimg, bg};
